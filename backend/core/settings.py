4503c4d643f06c42dab5bbd7bcc16dab5de1b803
robel <robelwon75@gmail.com>
2024-11-05 21:48:49 +0300

    custom user setup

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+AUTH_USER_MODEL = "users.UserAccount"
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -157,5 +157,5 @@ DJOSER = {
     'USER_CREATE_PASSWORD_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_RETYPE': True,
     'TOKEN_MODEL': None,
-    
+
 }
